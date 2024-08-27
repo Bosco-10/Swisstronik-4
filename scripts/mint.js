56e93314bdecc0c33eb58e2c7c2277cf17e5700d
root <root@vmi1997115.contaboserver.net>
2024-08-27 21:40:16 +0200

    Contribution to Swisstronik task

---
+++ scripts/mint.js
@@ -0,0 +1,35 @@
+const hre = require("hardhat");
+
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x0E57AC003C8F46398F9EDc7a09F3d99c7ffb7a6d"
+  const [signer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("PERC20Sample");
+  const contract = contractFactory.attach(contractAddress);
+  const functionName = "mint100tokens";
+  const mint100TokensTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName),
+    0
+  );
+  await mint100TokensTx.wait();
+  console.log(`Transaction URL of Mint: https://explorer-evm.testnet.swisstronik.com/tx/${mint100TokensTx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
